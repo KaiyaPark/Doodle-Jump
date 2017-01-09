6a3005b209d10d494949328fc9c7b2a7e437de21
Love4653 <league@iMac.attlocal.net>
2017-01-08 17:24:39 -0800

    yay i started doodle jump

--- src/DoodleJump.java
+++ src/DoodleJump.java
@@ -1,4 +1,27 @@
+import javax.swing.JFrame;
 
 public class DoodleJump {
+	JFrame f = new JFrame();
+	final int width = 500;
+	final int height = 800;
+	GamePanel g = new GamePanel();
+
+	DoodleJump() {
+		setup();
+	}
+
+	void setup() {
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		f.add(g);
+		f.setVisible(true);
+		g.startGame();
+		f.setSize(width, height);
+		f.addKeyListener(g);
+	}
+
+	public static void main(String[] args) {
+		DoodleJump d = new DoodleJump();
+
+	}
 
 }
---
+++ src/GamePanel.java
@@ -0,0 +1,52 @@
+import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.Timer;
+
+public class GamePanel extends JPanel implements ActionListener, KeyListener{
+	Timer t = new Timer(1000 / 60, this);
+	int x = 250;
+	int y = 600;
+	GamePanel() {
+
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		repaint();
+
+	}
+
+	void startGame() {
+		t.start();
+	}
+public void paintComponent(Graphics g){
+	g.fillRect(x, y, 100, 100);
+	}
+
+@Override
+public void keyTyped(KeyEvent e) {
+		
+}
+
+@Override
+public void keyPressed(KeyEvent e) {
+if(e.getKeyCode()==KeyEvent.VK_LEFT){
+	x = x-10;
+}
+if(e.getKeyCode()==KeyEvent.VK_RIGHT){
+	x = x+10;
+}
+}
+
+@Override
+public void keyReleased(KeyEvent e) {
+
+	
+}
+}
